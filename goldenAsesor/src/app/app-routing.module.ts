1f491c7a7613edc3ec53ed584bd6b32be5a77a2c
Angelicavillamil <villamilabrl@gmail.com>
2019-06-25 20:11:01 -0500

    Martes 25 8:00 pm

---
+++ goldenAsesor/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'login', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ goldenAsesor/src/app/login/login.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+
+  constructor (
+    private navCtrl : NavController
+    )
+    {}
+  register(){
+    this.navCtrl.navigateForward('register');
+  }
+  login(){
+    this.navCtrl.navigateForward('login/tabs/tab1');
+  }
+}
